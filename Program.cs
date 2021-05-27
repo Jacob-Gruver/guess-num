882dbcb7e1db51be52d5316e08f9af9e456a8df0
Jacob-Gruver <gruvinbro@gmail.com>
2021-05-27 10:51:18 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace guess_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Boolean won = false;
+
+            while (won == false)
+            {
+                Console.WriteLine("Choose a number between 0 - 100.");
+
+                string num = Console.ReadLine();
+
+                System.Console.WriteLine("You chose the number " + num);
+
+                if (num == "12")
+                {
+                    won = true;
+                    System.Console.WriteLine("You won!");
+                }
+
+                Console.Clear();
+
+            }
+        }
+    }
+}
